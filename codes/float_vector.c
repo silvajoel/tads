d6c04ba223e75917c6c141fe84d2c3664f0d03af
Joel Silva <silvajoel06@gmail.com>
2026-08-27 21:56:04 -0300

    feat: Functions have been created to create and destroy a Floatvector.

--- codes/float_vector.c
+++ codes/float_vector.c
@@ -1,6 +1,29 @@
 #include "float_vector.h"
-#include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Hello World\n");
+typedef struct float_vector{
+    int capacity; // num máximo de elementos
+    int size; // quantidade de elementos armazenados atualmente
+    float *data; // vetor de floats
+} FloatVector;
+
+/**
+ * @brief Cria (aloca) um vetor de floats com uma dada capacidade
+ * @param capacity Capacidade do vetor
+ * @return FloatVector* Um vetor floats alocado/criado
+ */
+FloatVector *create(int capacity){
+    FloatVector *vec = (FloatVector*) calloc(1, sizeof(FloatVector));
+    vec->size = 0;
+    vec->capacity = capacity;
+    vec->data = (float*) calloc(capacity, sizeof(float));
+
+    return vec; 
+}
+
+void destroy(FloatVector **vec_ref){
+    FloatVector *vec = *vec_ref;
+
+    free(vec->data);
+    free(vec);
 }
